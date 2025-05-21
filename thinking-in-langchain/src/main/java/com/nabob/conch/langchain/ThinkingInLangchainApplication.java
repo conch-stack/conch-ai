c8e31c2ee0bb28f5461c9e8a42eecfe9406d39d7
zjz6b6 <407091038@qq.com>
2025-05-21 18:36:56 +0800

    init
    init
    
    init

---
+++ thinking-in-langchain/src/main/java/com/nabob/conch/langchain/ThinkingInLangchainApplication.java
@@ -0,0 +1,13 @@
+package com.nabob.conch.langchain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThinkingInLangchainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThinkingInLangchainApplication.class, args);
+    }
+
+}
